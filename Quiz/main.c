9b1dee3e74c0c28ed591d0975aef41fe872257db
Sayawan Devs <sayawan.devs@gmail.com>
2015-01-02 00:55:51 +0200

    Initial Commit

---
+++ Quiz/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Quiz
+//
+//  Created by Rubyist on 02/01/15.
+//  Copyright (c) 2015 Sayawan Devs. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
